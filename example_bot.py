1b8e21d1f83fdf65896ca05c1d790b337f858d70
PixelPr0phet <abcantrell09@gmail.com>
2024-01-30 00:43:09 -0500

    experiment with quotes

--- example_bot.py
+++ example_bot.py
@@ -1,6 +1,8 @@
 import discord
 import config
 import logging
+import requests
+import json
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
@@ -8,6 +10,13 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+def get_quote():
+    response = requests.get
+    ("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = f"{json_data[0]['q']} - {json_data[0]['a']}"
+    return(quote)
 
 @client.event
 async def on_ready():
@@ -18,7 +27,8 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('$inspire'):
+        quote = get_quote()
+        await message.channel.send(quote)
 
 client.run(config.altr_token, log_handler=handler, log_level=logging.DEBUG)
